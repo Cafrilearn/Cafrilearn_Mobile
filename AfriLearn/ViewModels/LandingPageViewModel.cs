f3a4d359d8f858996f763297ca12644c50506535
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-01-27 08:57:27 +0300

    Refactored Landing Page

--- AfriLearn/ViewModels/LandingPageViewModel.cs
+++ AfriLearn/ViewModels/LandingPageViewModel.cs
@@ -9,5 +9,9 @@ namespace AfriLearn.ViewModels
     {
         public ICommand GoToClassRoom => new Command(() => NavigationService.PushAsync(new ExploreBooksPage()));
         public ICommand GoToReviewsPage => new Command(() => NavigationService.PushAsync(new ReviewsPage()));
+        public ICommand GoToDiscussionPage => new Command(() => NavigationService.PushAsync(new ForumPage()));
+        
+        // change this last one to a page to explore non curriculum books
+        public ICommand GoToExploreBooksPage => new Command(() => NavigationService.PushAsync(new ExploreBooksPage()));
     }
 }
---
+++ AfriLearn/Views/Settings/TermsAndConditionsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AfriLearn.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AfriLearn.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TermsAndConditionsPage : ContentPage
+    {
+        public TermsAndConditionsPage()
+        {
+            InitializeComponent();
+            if (InternetService.Internet())
+            {
+                termsConditionsWebView.Source = "https://reaiot.com/cafrilearn/legal/terms_conditions";
+            }
+        }
+    }
+}
